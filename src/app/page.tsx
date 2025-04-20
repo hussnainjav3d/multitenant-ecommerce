c856e43210154eba16297acdd88b09e3a4508c9a
Hussnain Javed <hussnain.jav3d@gmail.com>
2025-04-20 15:24:39 +0500

    02 - customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
 import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 
 export default function Home() {
   return (
-    <Button>Test</Button>
+    <div className="p-4">
+<div className="flex flex-col text-6xl gap-y-4">
+    <div >
+      <Button variant="elevated">I am here</Button>
+    </div>
+    <div className="text-6xl ">
+      <Input placeholder="I am here"/>
+    </div>
+    <div className="text-6xl ">
+      <Progress value={50}/>
+    </div>
+    <div className="text-6xl ">
+      <Textarea placeholder="I am here"/>
+    </div>
+    <div className="text-6xl ">
+      <Checkbox />
+    </div>
+    </div>
+    </div>
   );
 }
